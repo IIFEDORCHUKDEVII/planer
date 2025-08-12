c9c69370a38f27ed9d8d8c646adc9852a540c531
Dmytro Haida <dmytro.h.biz@solid-sl.com>
2025-08-12 23:35:37 +0300

    list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,28 @@
+export const listPurpose = [
+  {
+    id: 1,
+    name: "Purpose 1",
+  },
+  {
+    id: 2,
+    name: "Purpose 2",
+  },
+  {
+    id: 3,
+    name: "Purpose 3",
+  },
+];
+
 export const Header = () => {
   return (
     <div className="headerBlock">
+      <h3>Counts purpose</h3>
+      <h2>{listPurpose.length}</h2>
+      <div>
+        {listPurpose.map((item) => (
+          <p key={item.id}>{item.name}</p>
+        ))}
+      </div>
       <button className="button">Add purpose</button>
     </div>
   );
