4efee8c90ee1356573051518d222051e64bb4f7d
Dmytro Haida <dmytro.h.biz@solid-sl.com>
2025-08-12 23:07:29 +0300

    fix 2

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,18 +1,9 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import { Header } from "../../components/Header/Header";
 
 export const HomeScreen = () => {
   const { tg } = useTelegram();
-
-  useEffect(() => {
-    tg.ready();
-    // tg.MainButton.setText("Close");
-    // tg.MainButton.show();
-    // tg.onEvent("mainButtonClicked", () => {
-    //   onCLose();
-    // });
-  }, []);
 
   return (
     <div>
