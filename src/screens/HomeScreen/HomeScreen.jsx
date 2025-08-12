3e883a3c48c4e332bb40f81ff1d931df90f78eed
Dmytro Haida <dmytro.h.biz@solid-sl.com>
2025-08-12 23:09:31 +0300

    fix 3

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
-import { useTelegram } from "../../hooks/useTelegram";
 import { Header } from "../../components/Header/Header";
 
 export const HomeScreen = () => {
-  const { tg } = useTelegram();
 
   return (
     <div>
