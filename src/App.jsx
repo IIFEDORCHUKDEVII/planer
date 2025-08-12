6cc44aa030b59c7ffdb357b72b5d47bfd9c00df1
Dmytro Haida <dmytro.h.biz@solid-sl.com>
2025-08-12 14:52:46 +0300

    mainButton

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+const tg = window.Telegram.WebApp;
+function App() {
+  useEffect(() => {
+    // Ініціалізація Telegram Web App
+    tg.ready();
+    tg.MainButton.show();
+    // Наприклад, кнопка для закриття Telegram Web App
+  }, []);
+  const onCLose = () => {
+    tg.close();
+  };
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={onCLose}>Close</button>
+    </div>
+  );
+}
+
+export default App;
